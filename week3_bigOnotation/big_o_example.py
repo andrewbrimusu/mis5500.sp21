9ea5f480e2a69fa974402ee80cf98b1071540347
EC2 Default User <ec2-user@ip-172-31-13-82.us-west-1.compute.internal>
2021-03-08 20:59:15 +0000

    exam review code

--- week3_bigOnotation/big_o_example.py
+++ week3_bigOnotation/big_o_example.py
@@ -12,9 +12,29 @@ for l in lst:
     
 print("\nmean: ", tot / num_elem)
 
-for l in range(1000):
-    for j in range(1000):
-        for k in range(1000):
-            for m in range(1000):
-                print(l * j * k * m, end=" ")
+# O(n^4)
+# for l in range(1000):
+#     for j in range(1000):
+#         for k in range(1000):
+#             for m in range(1000):
+#                 print(l * j * k * m, end=" ")
                  
+# for i in range(1000):
+#     for j in range(1000):
+#         print(i * j)
+        
+# for m in range(1000):
+#     for n in range(1000):
+#         print(m * n)
+        
+        
+        
+lst = [[1,2,3], [3,4,5], [5,6,7], [8,9,0]]
+
+total = 0
+ct = 0
+for row in lst:
+    for item in row:
+        total += item
+        ct += 1
+print("average: ", total / ct)
